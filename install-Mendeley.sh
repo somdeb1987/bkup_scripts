39e3654e6662d0cfcdf0020798e47603539b6a61
Somdeb Bandopadhyay <sb020287@gmail.com>
2021-06-21 10:27:57 +0800

    Update install-Mendeley.sh

--- install-Mendeley.sh
+++ install-Mendeley.sh
@@ -1,7 +1,7 @@
 sudo apt install gconf2
 sudo apt install python
 mkdir -p tmp_mendeley
-cd tmp_google
+cd tmp_mendeley
 wget https://www.mendeley.com/repositories/ubuntu/stable/amd64/mendeleydesktop-latest
 sudo dpkg -i mendeleydesktop-latest
 cd ..
